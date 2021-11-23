2cba36887830e16b65dc80a573270fcaf3f93763
msanchcr <miansacre@gmail.com>
2021-11-23 20:15:05 +0100

    Added more testing to OrderFactory, not completed yet.

--- src/main/java/org/unir/core/OrderFactory.java
+++ src/main/java/org/unir/core/OrderFactory.java
@@ -1,5 +1,7 @@
 package org.unir.core;
 
+import java.util.Arrays;
+
 /**
  * This class is responsible for validating new order requests, creating them and 'saving' to a collection.
  * It uses singleton pattern in order to ensure there is only one instance of it.
@@ -17,22 +19,52 @@ public final class OrderFactory {
      * @param name the medicine name.
      * @param type the medicine type (uses an enum).
      * @param amount the total amount of medicine units ordered.
-     * @param delivery the company who is responsible for delivering the medicine.
+     * @param distributor the company who is responsible for delivering the medicine.
      * @param branch where the medicine should be delivered.
      *
      * @return the new order.
      */
-    public Order create (String name, String type, Integer amount, String delivery, String branch) {
-        //TODO --> Validate input parameters before creating new Order();
-        if (true
-        ) {
-            Order order = new Order(name, type, amount, delivery, branch);
-            OrderCollection.getInstance().getCollection().add(order);
-            return order;
-        } else {
-            //TODO --> raise exception if input parameters are not valid to create new Order();
-            return null;
+    public Order create (String name, String type, Integer amount, String distributor, String branch)
+        throws IllegalArgumentException {
+        if (name == null || name.isEmpty() || !name.matches("[a-zA-Z0-9]+")) {
+            throw new IllegalArgumentException(String.format(
+                    "Expected a medicine name using the regular expression [a-zA-Z0-9]+, but got %s.",
+                    name
+            ));
         }
+        if (type == null || type.isEmpty() || !MedicineEnum.exists(type)) {
+            throw new IllegalArgumentException(String.format(
+                "Expected type of medicine [%s], but got %s.",
+                MedicineEnum.valuesToString(),
+                type
+            ));
+        }
+        if (amount == null || amount <= 0) {
+            throw new IllegalArgumentException(String.format(
+                "Expected medicine amount <0, but got %d.",
+                amount
+            ));
+        }
+        if (distributor == null || distributor.isEmpty() || !DistributorEnum.exists(distributor)) {
+            throw new IllegalArgumentException(String.format(
+                "Expected medicine distributor [%s], but got %s.",
+                DistributorEnum.valuesToString(),
+                distributor
+            ));
+        }
+        if (branch == null) {
+            throw new IllegalArgumentException("Expected branch, got null.");
+        }
+        Order order = new Order(
+            name,
+            MedicineEnum.valueOf(type.toUpperCase()),
+            amount,
+            DistributorEnum.valueOf(distributor.toUpperCase()),
+            branch
+        );
+        OrderCollection.getInstance();
+        OrderCollection.getCollection().add(order);
+        return order;
     }
 
     /**
--- src/test/java/org/unir/core/OrderFactoryTest.java
+++ src/test/java/org/unir/core/OrderFactoryTest.java
@@ -1,17 +1,169 @@
 package org.unir.core;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class OrderFactoryTest {
+
+    private OrderFactory factory;
+
+    @BeforeEach
+    public void setUp () {
+        factory = OrderFactory.getInstance();
+    }
 
     /**
      * This test gets two (2) instances of OrderFactory in order to check that singleton works.
      */
     @Test
-    public void getFactoryInstance () {
+    public void getFactorySingleton () {
         OrderFactory factory = OrderFactory.getInstance();
         OrderFactory factory2 = OrderFactory.getInstance();
-        Assertions.assertEquals(factory, factory2);
+        Assertions.assertEquals(
+            factory.hashCode(),
+            factory2.hashCode()
+        );
+    }
+
+    /**
+     * This test checks all possible validations when trying to build an Order correctly.
+     */
+    @Test
+    public void validateOrderCorrect () {
+        Order correctOrder;
+        try {
+            Assertions.assertTrue(MedicineEnum.exists("PAINKILLER"));
+            correctOrder = factory.create("sudorixina", "PAINKILLER", 1, "Cofarma", "branch");
+            Assertions.assertNotNull(correctOrder);
+            correctOrder = factory.create("SUDORIZINA", "Painkiller", 2, "COFARMA", "branch");
+            Assertions.assertNotNull(correctOrder);
+            correctOrder = factory.create("123", "Anesthetic", 51234, "cOFarma", "branch");
+            Assertions.assertNotNull(correctOrder);
+            correctOrder = factory.create("Sudorixina", "PAINKILLER", 16, "Empsephar", "branch");
+            Assertions.assertNotNull(correctOrder);
+            correctOrder = factory.create("Sudorixina123", "Antacid", 1, "EMPSEPHAR", "branch");
+            Assertions.assertNotNull(correctOrder);
+            correctOrder = factory.create("S", "ANTACID", 32, "empSEPHAR", "branch");
+            Assertions.assertNotNull(correctOrder);
+            correctOrder = factory.create("s", "ANtacID", 25, "Cemefar", "branch");
+            Assertions.assertNotNull(correctOrder);
+            correctOrder = factory.create("1", "PAINKILLER", 128, "CEMEFAR", "branch");
+            Assertions.assertNotNull(correctOrder);
+            correctOrder = factory.create("Sa1", "PAINKILLER", 1, "cEMEfar", "branch");
+            Assertions.assertNotNull(correctOrder);
+        } catch (IllegalArgumentException e) {
+            Assertions.fail("No exception was expected.");
+        }
+    }
+
+    /**
+     * This test shows how medicine name validation works.
+     */
+    @Test
+    public void validateOrderIncorrectMedicineName () {
+        Assertions.assertThrows(
+            IllegalArgumentException.class,
+            () -> factory.create(null, "PAINKILLER", 1, "Cofarma", "branch"),
+            "Expected factory.create() to throw an IllegalArgumentException."
+        );
+        Assertions.assertThrows(
+            IllegalArgumentException.class,
+            () -> factory.create("", "PAINKILLER", 1, "Cofarma", "branch"),
+            "Expected factory.create() to throw an IllegalArgumentException."
+        );
+        Assertions.assertThrows(
+            IllegalArgumentException.class,
+            () -> factory.create("$", "PAINKILLER", 1, "Cofarma", "branch"),
+            "Expected factory.create() to throw an IllegalArgumentException."
+        );
+    }
+
+    /**
+     * This test shows how medicine type validation works.
+     */
+    @Test
+    public void validateOrderIncorrectMedicineType () {
+        Assertions.assertThrows(
+            IllegalArgumentException.class,
+            () -> factory.create("sudorixina", null, 1, "Cofarma", "branch"),
+            "Expected factory.create() to throw an IllegalArgumentException."
+        );
+        Assertions.assertThrows(
+            IllegalArgumentException.class,
+            () -> factory.create("sudorixina", "", 1, "Cofarma", "branch"),
+            "Expected factory.create() to throw an IllegalArgumentException."
+        );
+        Assertions.assertThrows(
+            IllegalArgumentException.class,
+            () -> factory.create("sudorixina", "nonExistingTypeOfMedicine", 1, "Cofarma", "branch"),
+            "Expected factory.create() to throw an IllegalArgumentException."
+        );
+    }
+
+    /**
+     * This test shows how amount validation works.
+     */
+    @Test
+    public void validateOrderIncorrectAmount () {
+        Assertions.assertThrows(
+            IllegalArgumentException.class,
+            () -> factory.create("sudorixina", "PAINKILLER", null, "Cofarma", "branch"),
+            "Expected factory.create() to throw an IllegalArgumentException."
+        );
+        Assertions.assertThrows(
+            IllegalArgumentException.class,
+            () -> factory.create("sudorixina", "PAINKILLER", -1, "Cofarma", "branch"),
+            "Expected factory.create() to throw an IllegalArgumentException."
+        );
+        Assertions.assertThrows(
+            IllegalArgumentException.class,
+            () -> factory.create("sudorixina", "PAINKILLER", 0, "Cofarma", "branch"),
+            "Expected factory.create() to throw an IllegalArgumentException."
+        );
+    }
+
+    /**
+     * This test shows how distributor validation works.
+     */
+    @Test
+    public void validateOrderIncorrectDistributor () {
+        Assertions.assertThrows(
+            IllegalArgumentException.class,
+            () -> factory.create("sudorixina", "PAINKILLER", 1, null, "branch"),
+            "Expected factory.create() to throw an IllegalArgumentException."
+        );
+        Assertions.assertThrows(
+            IllegalArgumentException.class,
+            () -> factory.create("sudorixina", "PAINKILLER", 1, "", "branch"),
+            "Expected factory.create() to throw an IllegalArgumentException."
+        );
+        Assertions.assertThrows(
+            IllegalArgumentException.class,
+            () -> factory.create("sudorixina", "PAINKILLER", 1, "nonExistingDistributor", "branch"),
+            "Expected factory.create() to throw an IllegalArgumentException."
+        );
+    }
+
+    /**
+     * This test shows how branch validation works.
+     */
+    @Disabled("This test case is disabled until Order.branch is correctly coded.")
+    @Test
+    public void validateOrderIncorrectBranch () {
+        Assertions.assertThrows(
+            IllegalArgumentException.class,
+            () -> factory.create("sudorixina", "PAINKILLER", 1, "Cofarma", "branch"),
+            "Expected factory.create() to throw an IllegalArgumentException."
+        );
+    }
+
+    /**
+     * This tests shows how to work with regular expressions in Strings.
+     */
+    @Test
+    public void regularExp() {
+        Assertions.assertTrue("sudorixina".matches("[a-z]+"));
     }
 }
