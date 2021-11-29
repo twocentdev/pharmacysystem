59b85cb0754ebe37b24bafc36d4c239f19128e9c
msanchcr <miansacre@gmail.com>
2021-11-29 17:09:16 +0100

    Cleaned project for Eclipse and created both MainView and PharmacyView.

--- src/main/java/org/unir/view/MainView.java
+++ src/main/java/org/unir/view/MainView.java
@@ -1,54 +1,59 @@
 package org.unir.view;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-public class MainView {
+public class MainView extends JFrame {
 
-    private final String JFRAME_TITLE = "Main view";
-    private final String JBUTTON_PHARMACY = "Pharmacy";
-    private final String JBUTTON_DISTRIBUTOR = "Distributor";
+	private final String VIEW_TITLE = "Main view";
+	private final String JBUTTON_PHARMACY = "Pharmacy";
+	private final String JBUTTON_DISTRIBUTOR = "Distributor";
+	
+	private static MainView instance;
+	private JPanel contentPane;
 
-    public static MainView instance;
-    JFrame frame;
+	/**
+	 * Create the frame.
+	 */
+	public MainView() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JButton pharmacyButton = new JButton(JBUTTON_PHARMACY);
+		pharmacyButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.out.println("Pharmacy button has been clicked.");
+				setVisible(false);
+				PharmacyView pharmacyView = new PharmacyView();
+				pharmacyView.setVisible(true);
+			}
+		});
+		contentPane.add(pharmacyButton);
+		
+		JButton distributorButton = new JButton(JBUTTON_DISTRIBUTOR);
+		distributorButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.out.println("Distributor button has been clicked.");
+			}
+		});
+		contentPane.add(distributorButton);
+	}
 
-    private void initView () {
-        frame = new JFrame(JFRAME_TITLE);
-        JPanel panel = new JPanel();
-        JButton pharmacyButton = new JButton(JBUTTON_PHARMACY);
-        JButton distributorButton = new JButton(JBUTTON_DISTRIBUTOR);
-
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300, 300);
-        frame.getContentPane().add(panel);
-
-        panel.setLayout( new FlowLayout() );
-        panel.add(pharmacyButton);
-        panel.add(distributorButton);
-
-        pharmacyButton.addActionListener(e -> {
-            PharmacyView pharmacyView = PharmacyView.getInstance();
-            pharmacyView.setVisible(true);
-            frame.setVisible(false);
-        });
-        distributorButton.addActionListener(e -> {
-            DistributorView distributorView = DistributorView.getInstance();
-            distributorView.setVisible(true);
-            frame.setVisible(false);
-        });
-
-        frame.setVisible(true);
-    }
-
-    public static MainView getInstance() {
-        if (instance == null) {
-            instance = new MainView();
-            instance.initView();
-        }
-        return instance;
-    }
-
-    public void setVisible (boolean visible) {
-        this.frame.setVisible(visible);
-    }
+	public static MainView getInstance () {
+		if (instance == null) {
+			instance = new MainView();
+		}
+		return instance;
+	}
 }
--- src/main/java/org/unir/view/PharmacyView.java
+++ src/main/java/org/unir/view/PharmacyView.java
@@ -1,14 +1,32 @@
 package org.unir.view;
 
-import org.unir.core.*;
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.border.EmptyBorder;
+
+import org.unir.core.DistributorEnum;
+import org.unir.core.MedicineEnum;
+import org.unir.core.Order;
+import org.unir.core.OrderFactory;
 import org.unir.util.Util;
 
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.List;
-import javax.swing.*;
-import java.awt.*;
 
-public class PharmacyView {
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+
+public class PharmacyView extends JFrame {
 
     private final String JFRAME_TITLE = "Pharmacy view";
     private final String JBUTTON_SUBMIT = "Submit";
@@ -21,154 +39,157 @@ public class PharmacyView {
     private final String JLABEL_FIELD_DIRECTIONS = "Directions: ";
     private final String JLABEL_DIR_1 = "Calle de la Rosa 28";
     private final String JLABEL_DIR_2 = "Calle Alcazabilla 3";
-    private final int JTEXTFIELD_WIDTH = 15;
+	
+	private static PharmacyView instance;
+	private JPanel contentPane;
+	private JTextField textFieldName;
+	private JTextField textFieldAmount;
 
-    public static PharmacyView instance;
-    JFrame frame;
-
-    private void initView () {
-        frame = new JFrame(JFRAME_TITLE);
-        JPanel panelMain = new JPanel();
-        JPanel panelForm = new JPanel();
-        JPanel panelNav = new JPanel();
-        JPanel panelFieldName = new JPanel();
-        JPanel panelFieldType = new JPanel();
-        JPanel panelFieldAmount = new JPanel();
-        JPanel panelFieldDistributor = new JPanel();
-        JPanel panelFieldDirections = new JPanel();
-        JPanel panelActionButtons = new JPanel();
-        JLabel labelFieldName = new JLabel(JLABEL_FIELD_NAME);
-        JLabel labelFieldType = new JLabel(JLABEL_FIELD_TYPE);
-        JLabel labelFieldAmount = new JLabel(JLABEL_FIELD_AMOUNT);
-        JLabel labelFieldDistributor = new JLabel(JLABEL_FIELD_DISTRIBUTOR);
-        JLabel labelFieldDirections = new JLabel(JLABEL_FIELD_DIRECTIONS);
-        JTextField textFieldName = new JTextField(JTEXTFIELD_WIDTH);
-        JComboBox fieldType = new JComboBox();
-        JTextField textFieldAmount = new JTextField(JTEXTFIELD_WIDTH);
-        ButtonGroup fieldDistributor = new ButtonGroup();
-        JCheckBox direction1 = new JCheckBox(JLABEL_DIR_1);
-        JCheckBox direction2 = new JCheckBox(JLABEL_DIR_2);
-        JButton submit = new JButton(JBUTTON_SUBMIT);
-        JButton clear = new JButton(JBUTTON_CLEAR);
-        JButton back = new JButton(JBUTTON_BACK);
-
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 500);
-        frame.getContentPane().add(panelMain);
-
-        panelMain.setLayout(new BorderLayout());
-        panelMain.add(panelForm, BorderLayout.CENTER);
-        panelMain.add(panelNav, BorderLayout.PAGE_END);
-
-        panelForm.setLayout(new BoxLayout(panelForm, BoxLayout.PAGE_AXIS));
-        panelForm.add(panelFieldName);
-        panelForm.add(panelFieldType);
-        panelForm.add(panelFieldAmount);
-        panelForm.add(panelFieldDistributor);
-        panelForm.add(panelFieldDirections);
-        panelForm.add(panelActionButtons);
-
-        panelFieldName.setLayout(new FlowLayout());
-        panelFieldName.add(labelFieldName);
-        panelFieldName.add(textFieldName);
-
-        panelFieldType.setLayout(new FlowLayout());
-        panelFieldType.add(labelFieldType);
-        panelFieldType.add(fieldType);
-        for (MedicineEnum medicine: MedicineEnum.values()) {
+	/**
+	 * Create the frame.
+	 */
+	public PharmacyView() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel panelMain = new JPanel();
+		contentPane.add(panelMain, BorderLayout.CENTER);
+		panelMain.setLayout(new BoxLayout(panelMain, BoxLayout.PAGE_AXIS));
+		
+		JPanel panelForm = new JPanel();
+		panelMain.add(panelForm);
+		panelForm.setLayout(new BoxLayout(panelForm, BoxLayout.PAGE_AXIS));
+		
+		JPanel panelFieldName = new JPanel();
+		panelForm.add(panelFieldName);
+		
+		JLabel labelFieldName = new JLabel(JLABEL_FIELD_NAME);
+		panelFieldName.add(labelFieldName);
+		
+		textFieldName = new JTextField();
+		textFieldName.setText("");
+		panelFieldName.add(textFieldName);
+		textFieldName.setColumns(10);
+		
+		JPanel panelFieldType = new JPanel();
+		panelForm.add(panelFieldType);
+		
+		JLabel labelFieldType = new JLabel(JLABEL_FIELD_TYPE);
+		panelFieldType.add(labelFieldType);
+		
+		JComboBox fieldType = new JComboBox();
+		panelFieldType.add(fieldType);
+		for (MedicineEnum medicine: MedicineEnum.values()) {
             fieldType.addItem(medicine.getMedicineName());
         }
-
-        panelFieldAmount.setLayout(new FlowLayout());
-        panelFieldAmount.add(labelFieldAmount);
-        panelFieldAmount.add(textFieldAmount);
-
-        panelFieldDistributor.setLayout(new FlowLayout());
-        panelFieldDistributor.add(labelFieldDistributor);
+		
+		JPanel panelFieldAmount = new JPanel();
+		panelForm.add(panelFieldAmount);
+		
+		JLabel labelFieldAmount = new JLabel(JLABEL_FIELD_AMOUNT);
+		panelFieldAmount.add(labelFieldAmount);
+		
+		textFieldAmount = new JTextField();
+		panelFieldAmount.add(textFieldAmount);
+		textFieldAmount.setColumns(10);
+		
+		JPanel panelFieldDistributor = new JPanel();
+		panelForm.add(panelFieldDistributor);
+		
+		JLabel labelFieldDistributor = new JLabel(JLABEL_FIELD_DISTRIBUTOR);
+		panelFieldDistributor.add(labelFieldDistributor);
+        ButtonGroup fieldDistributor = new ButtonGroup();
         for (DistributorEnum distributor: DistributorEnum.values()) {
             JRadioButton radioButton = new JRadioButton(distributor.getDistributorName());
             radioButton.setActionCommand(distributor.getDistributorName());
             fieldDistributor.add(radioButton);
             panelFieldDistributor.add(radioButton);
         }
-
-        panelFieldDirections.setLayout(new FlowLayout());
-        panelFieldDirections.add(labelFieldDirections);
+		
+		JPanel panelFieldDirections = new JPanel();
+		panelForm.add(panelFieldDirections);
+		
+		JLabel labelFieldDirections = new JLabel(JLABEL_FIELD_DIRECTIONS);
+		panelFieldDirections.add(labelFieldDirections);
+        JCheckBox direction1 = new JCheckBox(JLABEL_DIR_1);
+        JCheckBox direction2 = new JCheckBox(JLABEL_DIR_2);
         panelFieldDirections.add(direction1);
         panelFieldDirections.add(direction2);
+		
+		JPanel panelActions = new JPanel();
+		panelMain.add(panelActions);
+		
+		JButton cancel = new JButton(JBUTTON_CLEAR);
+		
+		JButton submit = new JButton(JBUTTON_SUBMIT);
+		submit.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+	            List<String> directions = new ArrayList<String> ();
+	            if (direction1.isSelected()) {
+	                directions.add(direction1.getText());
+	            }
+	            if (direction2.isSelected()) {
+	                directions.add(direction2.getText());
+	            }
+	            OrderFactory factory = OrderFactory.getInstance();
+	            try {
+	                Order order = factory.create(
+	                    (textFieldName.getText() == null) ? null : textFieldName.getText(),
+	                    (fieldType.getSelectedItem() == null)? null : fieldType.getSelectedItem().toString(),
+	                    Util.parseInt(textFieldAmount.getText()),
+	                    (fieldDistributor.getSelection() == null)? null : fieldDistributor.getSelection().getActionCommand(),
+	                    directions
+	                );
+	                System.out.println("New order has been created.");
+	                System.out.println(order);
+	            } catch (IllegalArgumentException ex) {
+	            	System.out.println("An error occurred while creating the order.");
+	                System.out.println(ex.getMessage());
+	            }
+			}
+		});
+		panelActions.add(submit);
+		
+		cancel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.out.println("Clear button has been clicked.");
+				textFieldName.setText(null);
+	            fieldType.setSelectedIndex(0);
+	            textFieldAmount.setText(null);
+	            fieldDistributor.clearSelection();
+	            direction1.setSelected(false);
+	            direction2.setSelected(false);
+			}
+		});
+		panelActions.add(cancel);
+		
+		JPanel panelNav = new JPanel();
+		contentPane.add(panelNav, BorderLayout.SOUTH);
+		
+		JButton back = new JButton(JBUTTON_BACK);
+		back.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.out.println("Back button has been clicked.");
+				setVisible(false);
+				MainView view = MainView.getInstance();
+				view.setVisible(true);
+			}
+		});
+		panelNav.add(back);
+	}
+	
+	public static PharmacyView getInstance () {
+		if (instance == null) {
+			instance = new PharmacyView ();
+		}
+		return instance;
+	}
 
-        direction1.setActionCommand(JLABEL_DIR_1);
-        direction2.setActionCommand(JLABEL_DIR_2);
-
-        panelActionButtons.setLayout(new FlowLayout());
-        panelActionButtons.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
-        panelActionButtons.add(submit);
-        panelActionButtons.add(clear);
-
-        panelNav.setLayout(new FlowLayout());
-        panelNav.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
-        panelNav.add(back);
-
-        submit.addActionListener( e -> {
-            List<String> directions = new ArrayList<String> ();
-            if (direction1.isSelected()) {
-                directions.add(direction1.getText());
-            }
-            if (direction2.isSelected()) {
-                directions.add(direction2.getText());
-            }
-            OrderFactory factory = OrderFactory.getInstance();
-            try {
-                Order order = factory.create(
-                    (textFieldName.getText() == null) ? null : textFieldName.getText(),
-                    (fieldType.getSelectedItem() == null)? null : fieldType.getSelectedItem().toString(),
-                    Util.parseInt(textFieldAmount.getText()),
-                    (fieldDistributor.getSelection() == null)? null : fieldDistributor.getSelection().getActionCommand(),
-                    directions
-                );
-                System.out.println(order);
-                JDialog orderDialog = new JDialog();
-                orderDialog.setLayout(new FlowLayout());
-                orderDialog.setSize(500, 100);
-                orderDialog.add(new JLabel(order.toString()));
-                orderDialog.setVisible(true);
-            } catch (IllegalArgumentException ex) {
-                System.out.println(ex.getMessage());
-                JDialog alertDialog = new JDialog();
-                alertDialog.setLayout(new FlowLayout());
-                alertDialog.setSize(500, 100);
-                alertDialog.add(new JLabel( ex.getMessage()));
-                alertDialog.setVisible(true);
-            } finally {
-                clear.doClick();
-            }
-        });
-        clear.addActionListener( e -> {
-            textFieldName.setText(null);
-            fieldType.setSelectedIndex(0);
-            textFieldAmount.setText(null);
-            fieldDistributor.clearSelection();
-            direction1.setSelected(false);
-            direction2.setSelected(false);
-        });
-        back.addActionListener( e -> {
-            MainView mainView = MainView.getInstance();
-            mainView.setVisible(true);
-            frame.setVisible(false);
-        });
-
-        frame.setVisible(true);
-    }
-
-    public static PharmacyView getInstance() {
-        if (instance == null) {
-            instance = new PharmacyView();
-            instance.initView();
-        }
-        return instance;
-    }
-
-    public void setVisible (boolean visible) {
-        this.frame.setVisible(visible);
-    }
 }
